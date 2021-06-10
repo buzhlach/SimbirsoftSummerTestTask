9f4c016d4ab31f3a37cf4d1cfc093fa7b0e84056
buzhlach <buzhlachudo@gmail.com>
2021-06-10 12:56:09 +0400

    Add project files.

---
+++ SimbirsoftTestTask/ParserURL.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SimbirsoftTestTask
+{
+    class ParserURL
+    {
+        /// <summary>
+        /// A string containing the site path
+        /// </summary>
+        private string site;
+
+        /// <summary>
+        /// Empty constructor with Google path
+        /// </summary>
+        public ParserURL()
+        {
+            this.site = @"https://www.google.com/";
+        }
+
+        /// <summary>
+        /// Constructor with site path
+        /// </summary>
+        /// <param name="site">Full path to the site</param>
+        public ParserURL(string site)
+        {
+            this.site = site;
+        }
+
+        /// <summary>
+        /// Parse Html page with regular expressions and split(SEPARATORS array)
+        /// </summary>
+        /// <returns>Dictionary with keys = words, values = counts</returns>
+        public Dictionary<string, int> Parse()
+        {
+            char[] SEPARATORS = { ' ', ',', '.', '!', '?', '\"', '\'', ';', ':', ']', '[', '(', ')', '\n', '\r', '\t', '+', '#', '&' };
+
+            WebClient client = new WebClient();
+            Stream inStream = client.OpenRead(site);
+            StreamReader reader = new StreamReader(inStream);
+
+            Dictionary<string, int> result = new Dictionary<string, int>();
+            string inputLine;
+            StringBuilder s = new StringBuilder();
+
+            while((inputLine = reader.ReadLine()) != null)
+            {
+                Regex regex = new Regex(">[^<]+(</script)|" +
+                    ">[^<]+(</style)|" +
+                    ">[^<]+< |" +
+                    ">[^<]+<=|" +
+                    ">[^<]+<");
+                MatchCollection matches = regex.Matches(inputLine);
+                foreach (Match match in matches)
+                {
+                    String subInputLine = match.Value;
+                    if ((!subInputLine.Contains("/script")) &&
+                            (!subInputLine.Contains("< ")) &&
+                            (!subInputLine.Contains("<=")) &&
+                            (!subInputLine.Contains("/style")))
+                    {
+                        subInputLine = subInputLine.Substring(1, subInputLine.Length - 2);
+                        s.Append(subInputLine + "\n");
+                        foreach (String word in subInputLine.Split(SEPARATORS))
+                        {
+                            if (!word.Equals(""))
+                            {
+                                if(result.TryGetValue(word.ToUpper(), out int getCount))
+                                {
+                                    result[word.ToUpper()]++;
+                                }
+                                else
+                                {
+                                    result.Add(word.ToUpper(), 1);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            reader.Close();
+            inStream.Close();
+            return result;
+        }
+    }
+}
---
+++ SimbirsoftTestTask/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SimbirsoftTestTask
+{
+    class Program
+    {
+        /// <summary>
+        /// Logs date, time and exception message to file log.txt 
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        static void Log(string message)
+        {
+            File.AppendAllText("log.txt", DateTime.Now.ToString() + " | " + message+'\n');
+        }
+        static void Main(string[] args)
+        {
+            string site = Console.ReadLine();
+            ParserURL myParserURL = new ParserURL(site);
+
+            try
+            {
+                Dictionary<string, int> words=myParserURL.Parse();
+                Console.Clear();
+                foreach(KeyValuePair<string,int> word in words.OrderBy(key => key.Value))
+                {
+                    Console.WriteLine("{0}: {1}",word.Key,word.Value);
+                }
+
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+                Log(e.Message);
+            }
+            catch(WebException e)
+            {
+                Console.WriteLine(e.Message);
+                Log(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Log(e.Message);
+            }
+        }
+    }
+}
